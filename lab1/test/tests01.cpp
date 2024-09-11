b0e876570cc18e69f410bd9978d7917c475fda79
APAndryukhin <apandryukhin@mai.education>
2024-09-11 22:21:13 +0300

    comple lab №1 try 2

--- lab1/test/tests01.cpp
+++ lab1/test/tests01.cpp
@@ -2,21 +2,36 @@
 #include "../include/countUnits.h"
 
 TEST(test_01, basic_test_set) {
-    ASSERT_TRUE(countUnits(2, 7) == 11);
+    int a = 2;
+    int b = 7;
+    int result = countUnits(a, b);
+    ASSERT_TRUE(result == 11);
 }
 
 TEST(test_02, basic_test_set) {
-    ASSERT_TRUE(countUnits(14, 101) == 298);
+    int a = 14;
+    int b = 101;
+    int result = countUnits(a, b);
+    ASSERT_TRUE(result == 298);
 }
 
 TEST(test_03, basic_test_set) {
-    ASSERT_TRUE(countUnits(0, 200) == 735);
+    int a = 0;
+    int b = 200;
+    int result = countUnits(0, 200);
+    ASSERT_TRUE(result == 735);
 }
 
 TEST(test_04, basic_test_set) {
-    ASSERT_TRUE(countUnits(2, 1) == 0);
+    int a = 2;
+    int b = 1;
+    int result = countUnits(2, 1);
+    ASSERT_TRUE(result == 0);
 }
 
 TEST(test_05, basic_test_set) {
-    ASSERT_TRUE(countUnits(5, 5) == 2);
+    int a = 5;
+    int b = 5;
+    int result = countUnits(5, 5);
+    ASSERT_TRUE(result == 2);
 }
