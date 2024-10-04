6a1e5dbc6a5782b428914e1015c67b58281076ee
APAndryukhin <apandryukhin@mai.education>
2024-10-04 18:58:31 +0000

    fix bug

--- lab2/test/tests01.cpp
+++ lab2/test/tests01.cpp
@@ -1,51 +1,104 @@
 #include <gtest/gtest.h>
 #include "../include/Decimal.hpp"
 
-
-TEST(test_01, basic_test_set) {
-    Decimal var1 = Decimal("20");
-    Decimal var2 = Decimal{'2', '0'};
-    Decimal var3 = Decimal(5, '2');
-    Decimal var4 = var1;
-    ASSERT_TRUE(true);
+TEST(DecimalTest, ConstructorAndPrint) {
+    Decimal var1("12345");
+    std::stringstream ss;
+    var1.print(ss);
+    ASSERT_EQ(ss.str(), "12345");
 }
 
-TEST(test_02, basic_test_set) {
-    Decimal var1 = Decimal("20");
-    Decimal var2 = Decimal("30");
-    Decimal var3 = var1;
-
-    bool equals = (var1 == var3); // true
-    bool not_equals = (var1 != var3); // false
-    ASSERT_TRUE(equals);
-    ASSERT_FALSE(not_equals);
-
-
-    bool greater1 = (var1 > var2); // false
-    bool greater2 = (var2 > var1); // true
-    ASSERT_FALSE(greater1);
-    ASSERT_TRUE(greater2);
-
-
-    bool less1 = (var1 < var2); // true;
-    bool less2 = (var2 < var1); // false
-    ASSERT_TRUE(less1);
-    ASSERT_FALSE(less2);
-}
-
-TEST(test_03, basic_test_set) {
-    Decimal var1 = Decimal("20");
-    Decimal var2 = Decimal("1000");
-    Decimal var3 = Decimal("10");
-
-    Decimal add_result = Decimal("1020");
-    Decimal remove_result = Decimal("990");
+TEST(DecimalTest, Addition) {
+    Decimal var1("12345");
+    Decimal var2("54321");
+    Decimal expected("66666");
 
     var1 += var2;
-    bool result1 = (add_result == var1);
-    ASSERT_TRUE(result1);
+    ASSERT_EQ(var1, expected);
+}
 
-    var2 -= var3;
-    bool result2 = (remove_result == var2);
-    ASSERT_TRUE(result2);
+TEST(DecimalTest, Subtraction) {
+    Decimal var1("54321");
+    Decimal var2("12345");
+    Decimal expected("41976");
+
+    var1 -= var2;
+    ASSERT_EQ(var1, expected);
+}
+
+TEST(DecimalTest, AdditionWithCarry) {
+    Decimal var1("999");
+    Decimal var2("1");
+    Decimal expected("1000");
+
+    var1 += var2;
+    ASSERT_EQ(var1, expected);
+}
+
+TEST(DecimalTest, SubtractionWithBorrow) {
+    Decimal var1("1000");
+    Decimal var2("1");
+    Decimal expected("999");
+
+    var1 -= var2;
+    ASSERT_EQ(var1, expected);
+}
+
+TEST(DecimalTest, Equality) {
+    Decimal var1("12345");
+    Decimal var2("12345");
+
+    ASSERT_TRUE(var1 == var2);
+    ASSERT_FALSE(var1 != var2);
+}
+
+TEST(DecimalTest, Comparison) {
+    Decimal var1("12345");
+    Decimal var2("54321");
+
+    ASSERT_TRUE(var1 < var2);
+    ASSERT_FALSE(var1 > var2);
+}
+
+TEST(DecimalTest, LeadingZeros) {
+    Decimal var("000123");
+    std::stringstream ss;
+    var.print(ss);
+    ASSERT_EQ(ss.str(), "123");
+}
+
+TEST(DecimalTest, Assignment) {
+    Decimal var1("12345");
+    Decimal var2 = var1;
+    Decimal var3 = std::move(var1);
+
+    std::stringstream ss;
+    var2.print(ss);
+    ASSERT_EQ(ss.str(), "12345");
+
+    ss.str("");
+    var3.print(ss);
+    ASSERT_EQ(ss.str(), "12345");
+}
+
+TEST(DecimalTest, AdditionSubtractionDifferentLengths) {
+    Decimal var1("9999");
+    Decimal var2("1");
+    Decimal expected_add("10000");
+    Decimal expected_sub("9999");
+
+    var1 += var2;
+    ASSERT_EQ(var1, expected_add);
+
+    var1 -= var2;
+    ASSERT_EQ(var1, expected_sub);
+}
+
+TEST(DecimalTest, LargeNumbers) {
+    Decimal var1("99999999999999999999");
+    Decimal var2("1");
+    Decimal expected("100000000000000000000");
+
+    var1 += var2;
+    ASSERT_EQ(var1, expected);
 }
